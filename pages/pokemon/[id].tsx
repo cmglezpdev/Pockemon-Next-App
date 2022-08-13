8c4a6a2a9d552bb281249f556b6b53b0ce9c195a
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-08-13 01:14:22 -0400

    add details cards of the pokemons

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -2,7 +2,10 @@ import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
 import React from 'react';
 import { MainLayout } from '../../components/layout';
 import pokeApi from '../../api/pokeApi';
-import { PokemonDetail } from '../../interfaces/pokemon-detail';
+import { PokemonDetail, Sprites } from '../../interfaces/pokemon-detail';
+import { Button, Card, Container, Grid, Text } from '@nextui-org/react';
+import Head from 'next/head';
+import Image from 'next/image';
 
 interface Props {
     pokemon: PokemonDetail;
@@ -12,7 +15,64 @@ const PokemonPage:NextPage<Props> = ({ pokemon }) => {
 
     return (
         <MainLayout title='Listado de Pokemons'>
-            <h1>{ pokemon.name }</h1>
+            <Grid.Container css={{marginTop: '5px'}} gap={2}>
+                <Grid xs={12} sm={4}>
+                    <Card isHoverable css={{ padding: '30px' }}>
+                        <Card.Body>
+                            <Card.Image
+                                src={pokemon.sprites.other?.dream_world.front_default|| 'no-image.png'}
+                                alt={pokemon.name}
+                                width="100%"
+                                height="200px"
+                            />
+                        </Card.Body>
+                    </Card>
+                </Grid>
+                <Grid xs={12} sm={8}>
+                    <Card>
+                        <Card.Header css={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <Text h1 transform='capitalize'>{ pokemon.name }</Text>
+                        
+                            <Button
+                                color='gradient'
+                                ghost
+                            >
+                                Favorite
+                            </Button>
+                        </Card.Header>
+                        <Card.Body>
+                            <Text size={30}>Sprites</Text> 
+                            <Container direction='row' display='flex' gap={0} justify='space-evenly'>
+                                <Image 
+                                    src={pokemon.sprites.front_default} 
+                                    alt={ pokemon.name } 
+                                    width={100} 
+                                    height={100} 
+                                />
+                                <Image 
+                                    src={pokemon.sprites.back_default} 
+                                    alt={ pokemon.name } 
+                                    width={100} 
+                                    height={100} 
+                                />
+                                <Image 
+                                    src={pokemon.sprites.front_shiny} 
+                                    alt={ pokemon.name } 
+                                    width={100} 
+                                    height={100} 
+                                />
+                                <Image 
+                                    src={pokemon.sprites.back_shiny} 
+                                    alt={ pokemon.name } 
+                                    width={100} 
+                                    height={100} 
+                                />
+
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </Grid>
+            </Grid.Container>
         </MainLayout>
     )
 };
